9da8330659ea0f0689e027dbcf48d2bd5207b52f
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-02-20 17:02:43 +0100

    jauge

--- monJeu.js
+++ monJeu.js
@@ -21,14 +21,18 @@ var game = new Phaser.Game(config);
 var score = 0;
 var saut = 2;
 var press = 0;
+var pv;
+var maxPv;
 
 function init(){
  	var platforms;
 	var player;
-	var cursors; 
+	var cursors;
 	var stars;
 	var scoreText;
+	var ptText;
 	var bomb;
+	
 }
 
 function preload(){
@@ -38,6 +42,8 @@ function preload(){
 	this.load.image('sol','assets/platform.png');
 	this.load.image('bomb','assets/Kings and Pigs/Sprites/09-Bomb/Bomb Off.png');
 	this.load.spritesheet('perso','assets/free-pixel-art-tiny-hero-sprites/1 Pink_Monster/Pink_Monster_Idle_4.png',{frameWidth: 32, frameHeight: 32});
+	this.load.image('pv_red','assets/health-red.png');
+	this.load.image('pv_green','assets/health-green.png');
 }
 
 
@@ -53,12 +59,15 @@ function create(){
 	platforms.create(600,400,'sol');
 	platforms.create(50,250,'sol');
 	
+	
 	player = this.physics.add.sprite(100,450,'perso');
 	player.setCollideWorldBounds(true);
 	player.setBounce(0.2);
 	player.body.setGravityY(000);
 	this.physics.add.collider(player,platforms);
-	
+	player.pv = 100;
+	player.maxPv = 100;
+
 	cursors = this.input.keyboard.createCursorKeys(); 
 	
 	this.anims.create({
@@ -79,11 +88,21 @@ function create(){
 		repeat:11,
 		setXY: {x:12,y:0,stepX:70}
 	});
+
 	
+
+	pvGreen = this.physics.add.staticGroup();
+	pvGreen.create(680,80,'pv_green');
+
+	pvRed = this.physics.add.staticGroup();
+	//pvRed.create(680,80,'pv_red');
+	//pvRed.create(580,70,'pv_red').setScale(0.1,1).setOrigin(0,0);
+
 	this.physics.add.collider(stars,platforms);
 	this.physics.add.overlap(player,stars,collectStar,null,this);
 
 	scoreText = this.add.text(16,16, 'score: 0', {fontSize: '32px', fill:'#000'});
+	ptText = this.add.text(600,16, 'Vie : 100', {fontSize: '32px', fill:'#000'});
 	bombs = this.physics.add.group();
 	this.physics.add.collider(bombs,platforms);
 	this.physics.add.collider(bombs,bombs);
@@ -108,10 +127,10 @@ function update(){
 		saut -= 1;
 		press -= 1;
 		if (saut == 1) {
-            player.setVelocityY(-230);
+            player.setVelocityY(-260);
         }
         if (saut == 0) {
-            player.setVelocityY(-230);
+            player.setVelocityY(-260);
         }
 	}
 	if(cursors.up.isUp){
@@ -119,14 +138,23 @@ function update(){
 	}
 	 if (cursors.up.isUp && player.body.touching.down) {
 		saut = 2;
-	} 
-	
+	}
+	if (player.pv <= 0){
+		this.physics.pause();
+		player.setTint(0xff0000);
+		player.anims.play('turn');
+		gameOver=true;
+	}
 }
+
 function hitBomb(player, bomb){
-	this.physics.pause();
-	player.setTint(0xff0000);
-	player.anims.play('turn');
-	gameOver=true;
+	bomb.disableBody(true,true);
+	createBomb();
+	score -= 100;
+	scoreText.setText('score: '+score);
+	player.pv = player.pv - 10;
+	ptText.setText('Vie : '+player.pv);
+	pvRed.create(560 + (100 - player.pv) * 2,70,'pv_red').setScale(0.1,1).setOrigin(0,0);
 }
 
 function collectStar(player, star){
@@ -138,7 +166,11 @@ function collectStar(player, star){
 			child.enableBody(true,child.x,0, true, true);
 		});
 		
-		var x = (player.x < 400) ? 
+		createBomb();
+	}
+}
+function createBomb(){
+	var x = (player.x < 400) ? 
 			Phaser.Math.Between(400,800):
 			Phaser.Math.Between(0,400);
 		var bomb = bombs.create(x, 16, 'bomb');
@@ -147,5 +179,4 @@ function collectStar(player, star){
 		bomb.setBounce(1);
 		bomb.setCollideWorldBounds(true);
 		bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-	}
 }
